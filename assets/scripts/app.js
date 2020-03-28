dcc7932cd7762756b0310c4881e08112386073d4
Hadi Arbabi <h.arbabi@banicm.com>
2020-03-28 13:49:26 +0430

    calculator-v1.1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,43 @@ function getUserInputNum() {
     return parseInt(userInput.value);
 }
 
-function add() {
-    const enteredNumber = getUserInputNum();
-    const currentCalculation = `${currentResult} + ${enteredNumber}`;
-    currentResult = currentResult + enteredNumber;
+function createLogOutput(oprator, resultBeforeCal, calNumber) {
+    const currentCalculation = `${resultBeforeCal} ${oprator} ${calNumber}`;
     outputResult(currentResult, currentCalculation);
 }
 
+function add() {
+    //if this button clicked without any number it shoud be nan an logical error ?????
+    const enteredNumber = getUserInputNum();    
+    const momentResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createLogOutput("+", momentResult, enteredNumber);
+}
+function subtract() {
+    const enteredNumber = getUserInputNum();
+    const momentResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createLogOutput("-", momentResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserInputNum();
+    const momentResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createLogOutput("*", momentResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserInputNum();
+    const momentResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createLogOutput("/", momentResult, enteredNumber);
+}
+
+function clear() {}
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+// clearbtn.addEventListener("click", clear);
