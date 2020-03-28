7af95c7a83f85a38d854774448a79e4e5e114cbb
Hadi Arbabi <h.arbabi@banicm.com>
2020-03-28 16:07:13 +0430

    calculator-v1.2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,41 +1,56 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = []; //literal for store log of oprator
 
 function getUserInputNum() {
-    return parseInt(userInput.value);
+    if (parseInt(userInput.value)) {
+        return parseInt(userInput.value);
+    }
+    return "enter first num"; //get input from input field in html
 }
 
+//create caculation log and show input numer in calnumber
 function createLogOutput(oprator, resultBeforeCal, calNumber) {
     const currentCalculation = `${resultBeforeCal} ${oprator} ${calNumber}`;
     outputResult(currentResult, currentCalculation);
 }
 
 function add() {
-    //if this button clicked without any number it shoud be nan an logical error ?????
-    const enteredNumber = getUserInputNum();    
-    const momentResult = currentResult;
-    currentResult = currentResult + enteredNumber;
-    createLogOutput("+", momentResult, enteredNumber);
+    //if this button clicked without any number it_
+    //_shoud be nan an logical error ?????
+    const enteredNumber = getUserInputNum();
+    const beforeResult = currentResult;
+    currentResult += enteredNumber;
+    createLogOutput("+", beforeResult, enteredNumber);
+    const logEntry = {
+        opration: "ADD",     //make an object from the 
+        prevResult: beforeResult,//last seen result
+        number: enteredNumber,//entered number
+        result: currentResult//new genereated result
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
 }
+
 function subtract() {
     const enteredNumber = getUserInputNum();
-    const momentResult = currentResult;
-    currentResult = currentResult - enteredNumber;
-    createLogOutput("-", momentResult, enteredNumber);
+    const beforeResult = currentResult;
+    currentResult -= enteredNumber;
+    createLogOutput("-", beforeResult, enteredNumber);
 }
 
 function multiply() {
     const enteredNumber = getUserInputNum();
-    const momentResult = currentResult;
-    currentResult = currentResult * enteredNumber;
-    createLogOutput("*", momentResult, enteredNumber);
+    const beforeResult = currentResult;
+    currentResult *= enteredNumber;
+    createLogOutput("*", beforeResult, enteredNumber);
 }
 
 function divide() {
     const enteredNumber = getUserInputNum();
-    const momentResult = currentResult;
-    currentResult = currentResult / enteredNumber;
-    createLogOutput("/", momentResult, enteredNumber);
+    const beforeResult = currentResult;
+    currentResult /= enteredNumber;
+    createLogOutput("/", beforeResult, enteredNumber);
 }
 
 function clear() {}
@@ -44,4 +59,3 @@ addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
 multiplyBtn.addEventListener("click", multiply);
 divideBtn.addEventListener("click", divide);
-// clearbtn.addEventListener("click", clear);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -3,7 +3,6 @@ const addBtn = document.getElementById("btn-add");
 const subtractBtn = document.getElementById("btn-subtract");
 const multiplyBtn = document.getElementById("btn-multiply");
 const divideBtn = document.getElementById("btn-divide");
-// const clearbtn = document.getElementById("btn-clear");
 
 const currentCalculationOutput = document.getElementById("current-calculation");
 const currentResultOutput = document.getElementById("current-result");
