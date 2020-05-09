3baf2e53b4de0c4a11737c30e620a5e3cd73fec8
Hadi Arbabi <h.arbabi@banicm.com>
2020-05-09 16:51:14 +0430

    format edired

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,48 +3,48 @@ let currentResult = defaultResult;
 let logEntries = []; //literal for store log of oprator
 
 function getUserInputNum() {
-    return parseInt(userInput.value);
+  return parseInt(userInput.value);
 }
 //create caculation log and show input numer in calnumber
 function createLogOutput(oprator, resultBeforeCal, calNumber) {
-    const currentCalculation = `${resultBeforeCal} ${oprator} ${calNumber}`;
-    outputResult(currentResult, currentCalculation);
+  const currentCalculation = `${resultBeforeCal} ${oprator} ${calNumber}`;
+  outputResult(currentResult, currentCalculation);
 }
 
 function writeToLog(oprator, prevresult, numbered, calresult) {
-    const logEntry = {
-        opration: oprator, //make an object from the
-        prevResult: prevresult, //last seen result
-        number: numbered, //entered number
-        result: calresult, //new genereated result
-    };
-    logEntries.push(logEntry);
-    console.log(logEntries);
+  const logEntry = {
+    opration: oprator, //make an object from the
+    prevResult: prevresult, //last seen result
+    number: numbered, //entered number
+    result: calresult, //new genereated result
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
 function calculation(calculationType) {
-    const enteredNumber = getUserInputNum();
-    if (!enteredNumber) {
-        //check the number is valid or not
-        return;
-    }
-    const beforeResult = currentResult;
-    let mathOprator;
-    if (calculationType === "ADD") {
-        currentResult += enteredNumber;
-        mathOprator = "+";
-    } else if (calculationType === "SUBTRACT") {
-        currentResult -= enteredNumber;
-        mathOprator = "-";
-    } else if (calculationType === "MULTIPLY") {
-        currentResult *= enteredNumber;
-        mathOprator = "*";
-    } else if (calculationType === "DIVIDE") {
-        currentResult /= enteredNumber;
-        mathOprator = "/";
-    }
-    createLogOutput(mathOprator, beforeResult, enteredNumber);
-    writeToLog(calculationType, beforeResult, enteredNumber, currentResult);
+  const enteredNumber = getUserInputNum();
+  if (!enteredNumber) {
+    //check the number is valid or not
+    return;
+  }
+  const beforeResult = currentResult;
+  let mathOprator;
+  if (calculationType === "ADD") {
+    currentResult += enteredNumber;
+    mathOprator = "+";
+  } else if (calculationType === "SUBTRACT") {
+    currentResult -= enteredNumber;
+    mathOprator = "-";
+  } else if (calculationType === "MULTIPLY") {
+    currentResult *= enteredNumber;
+    mathOprator = "*";
+  } else if (calculationType === "DIVIDE") {
+    currentResult /= enteredNumber;
+    mathOprator = "/";
+  }
+  createLogOutput(mathOprator, beforeResult, enteredNumber);
+  writeToLog(calculationType, beforeResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener("click", () => calculation("ADD"));
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -8,8 +8,8 @@ const currentCalculationOutput = document.getElementById("current-calculation");
 const currentResultOutput = document.getElementById("current-result");
 
 function outputResult(result, text) {
-    currentResultOutput.textContent = result;
-    currentCalculationOutput.textContent = text;
+  currentResultOutput.textContent = result;
+  currentCalculationOutput.textContent = text;
 }
 
 console.log("hey");
